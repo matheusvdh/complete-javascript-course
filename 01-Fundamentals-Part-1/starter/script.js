0a32d8ae94a4a3c17e7d565ca97124825606dd72
Matheus van der Ham <matheus.vdh@gmail.com>
2022-07-05 02:16:06 -0300

    lecture sobre data  types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,17 @@
+// LECTURE: Values and Variables
+
 let meuPais = "Brasil";
 let meuContiennte = "América do Sul";
 let populacaoBr = "210 milhões"
 
 console.log(`O meu país é o ${meuPais}, localizado no continente da ${meuContiennte} e contêm ${populacaoBr} de habitantes`);
+
+// LECTURE: Data Types
+
+let isIsland = false
+let language;
+
+language = "Portugues"
+
+
+console.log(`O meu país é uma ilha? ${isIsland}, o ${meuPais} contêm ${populacaoBr} de habitantes e sua lingua materna é o ${language}`);
