db1a93ff437d36fba6d6d05a911facc83b07e6e5
Matheus van der Ham <matheus.vdh@gmail.com>
2022-07-05 00:00:27 -0300

    andamento das aulas

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let meuPais = "Brasil";
+let meuContiennte = "América do Sul";
+let populacaoBr = "210 milhões"
+
+console.log(`O meu país é o ${meuPais}, localizado no continente da ${meuContiennte} e contêm ${populacaoBr} de habitantes`);
